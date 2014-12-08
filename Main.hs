ef6db1b4335beb449224ccf045357c428034f062
Eric Rochester <erochest@virginia.edu>
2014-12-08 14:05:22 -0500

    Command-line options.

--- Main.hs
+++ Main.hs
@@ -4,28 +4,82 @@
 module Main where
 
 
+import           Options.Applicative
 import           Graphics.Gloss
 import           Graphics.Gloss.Interface.IO.Simulate
 
 
-data Simulation = Simulation
+type Chronon = Int
+type Energy  = Int
+
+data Simulation
+        = Simulation
+        { params :: !Params
+        }
+
+data Params
+        = Params
+        { initialSharks  :: !Double
+        , initialFish    :: !Double
+        , sharkReproduce :: !Chronon
+        , fishReproduce  :: !Chronon
+        , initEnergy     :: !Energy
+        , fishBoost      :: !Energy
+        }
 
 
 render :: Simulation -> IO Picture
-render _ = putStrLn "render" >> return Blank
+render _ = return Blank
 
 step :: ViewPort -> Float -> Simulation -> IO Simulation
 step _ _ = return
 
 
 main :: IO ()
-main =
+main = do
+    ps <- execParser opts
     simulateIO (InWindow "Wa-Tor" (w, h) (0, 0))
                black
                10
-               Simulation
+               (Simulation ps)
                render
                step
     where
         w = 500
         h = 309
+
+opts' :: Parser Params
+opts' =   Params
+      <$> option auto
+                 (  long "initial-sharks"
+                 <> metavar "INITIAL_SHARKS_PROBABILITY" <> value 0.1
+                 <> help "The probability that an initial location will\
+                         \ be a shark.")
+      <*> option auto
+                 (  long "initial-fish"
+                 <> metavar "INITIAL_FISH_PROBABILITY" <> value 0.2
+                 <> help "The probability that an initial location will\
+                         \ be a fish.")
+      <*> option auto
+                 (  long "reproduce-shark"
+                 <> metavar "SHARK_REPRODUCE_AGE" <> value 10
+                 <> help "The age at which shark reproduce.")
+      <*> option auto
+                 (  long "reproduce-fish"
+                 <> metavar "FISH_REPRODUCE_AGE" <> value 10
+                 <> help "The age at which fish reproduce.")
+      <*> option auto
+                 (  long "initial-shark-energy"
+                 <> metavar "INITIAL_SHARK_ENERGY" <> value 15
+                 <> help "The amount of energy sharks are created with.")
+      <*> option auto
+                 (  long "fish-energy"
+                 <> metavar "EATEN_FISH_ENERGY" <> value 10
+                 <> help "The amount of energy a shark gets from eating\
+                         \ a fish.")
+
+opts :: ParserInfo Params
+opts = info (helper <*> opts')
+            (  fullDesc
+            <> progDesc "Wa-Tor population dynamics simulation."
+            <> header "wa-tor - population dynamics")
