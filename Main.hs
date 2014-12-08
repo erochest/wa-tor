7de12ee08a948dcc33e0a23723e9656a7f84a0ff
Eric Rochester <erochest@virginia.edu>
2014-12-08 13:48:41 -0500

    Blank screen.

--- Main.hs
+++ Main.hs
@@ -4,5 +4,28 @@
 module Main where
 
 
+import           Graphics.Gloss
+import           Graphics.Gloss.Interface.IO.Simulate
+
+
+data Simulation = Simulation
+
+
+render :: Simulation -> IO Picture
+render _ = putStrLn "render" >> return Blank
+
+step :: ViewPort -> Float -> Simulation -> IO Simulation
+step _ _ = return
+
+
 main :: IO ()
-main = undefined
+main =
+    simulateIO (InWindow "Wa-Tor" (w, h) (0, 0))
+               black
+               10
+               Simulation
+               render
+               step
+    where
+        w = 500
+        h = 309
