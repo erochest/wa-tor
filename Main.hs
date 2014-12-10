8e7ad49ed2cc25ea77308267c6bd02e1c7a7ce13
Eric Rochester <erochest@virginia.edu>
2014-12-10 09:41:32 -0500

    Stubbed out fish movement.

--- Main.hs
+++ Main.hs
@@ -5,10 +5,15 @@
 module Main where
 
 
-import           Control.Monad
+import Prelude hiding (mapM)
+import           Control.Arrow                        hiding (left, right)
+import           Control.Monad hiding (mapM)
 import           Data.Bifunctor
 import           Data.Maybe
+import           Data.Traversable
 import qualified Data.Vector                          as V
+import qualified Data.Vector.Generic                  as GV
+import qualified Data.Vector.Mutable                  as MV
 import           Graphics.Gloss
 import           Graphics.Gloss.Interface.IO.Simulate
 import           Options.Applicative
@@ -21,7 +26,8 @@ type Coord   = (Int, Int)
 
 data WaTor
         = WaTor
-        { world :: !(Vector2D Entity)
+        { time  :: !Chronon
+        , world :: !(Vector2D V.Vector Entity)
         }
 
 data Simulation
@@ -44,83 +50,191 @@ data Params
 
 data Entity
         = Fish
+        { fishAge :: !Chronon
+        }
         | Shark
+        { sharkAge    :: !Chronon
+        , sharkEnergy :: !Energy
+        }
         | Empty
-        deriving (Eq, Show, Enum, Bounded)
+        deriving (Eq, Show)
 
-data Vector2D a
+data Vector2D v a
         = V2D
         { v2dExtent :: !Coord
-        , v2dData   :: !(V.Vector a)
+        , v2dData   :: !(v a)
         }
 
-instance Functor Vector2D where
+instance Functor v => Functor (Vector2D v) where
     fmap f (V2D e d) = V2D e $ fmap f d
 
-(!?) :: Vector2D a -> Coord -> Maybe a
+(!?) :: Vector2D V.Vector a -> Coord -> Maybe a
 (V2D (w, _) d) !? (x, y) = d V.!? i
     where
         i = y * w + x
 
-fromList :: Coord -> [a] -> Vector2D a
+(!!?) :: Vector2D MV.IOVector a -> Coord -> IO (Maybe a)
+(V2D (w, _) d) !!? (x, y) | i >= MV.length d = return Nothing
+                          | otherwise        = Just <$> MV.read d i
+    where
+        i = y * w + x
+
+fromList :: Coord -> [a] -> Vector2D V.Vector a
 fromList extent = V2D extent . V.fromList
 
-toList :: Vector2D a -> [a]
+toList :: Vector2D V.Vector a -> [a]
 toList = V.toList . v2dData
+
+indexes :: Coord -> [Coord]
+indexes (w, h) = [ (x, y) | y <- [0..h-1], x <- [0..w-1] ]
+
+both :: (a -> b) -> (a, a) -> (b, b)
+both f = f `bimap` f
+
+floatPair :: Coord -> (Float, Float)
+floatPair = both fromIntegral
 
 
 randomWaTor :: GenIO -> Params -> Coord -> IO WaTor
 randomWaTor g p extent@(w, h) =
-    WaTor . V2D extent . V.fromList <$> replicateM (w * h) (randomEntity g p)
+    WaTor 0 . V2D extent . V.fromList <$> replicateM (w * h) (randomEntity g p)
 
 randomEntity :: GenIO -> Params -> IO Entity
 randomEntity g Params{..} = randomEntity' <$> uniform g
     where
-        randomEntity' x | x <= initialFish                   = Fish
-                        | x <= (initialFish + initialSharks) = Shark
+        randomEntity' x | x <= initialFish                   = Fish  0
+                        | x <= (initialFish + initialSharks) = Shark 0 initEnergy
                         | otherwise                          = Empty
 
 render :: Simulation -> IO Picture
-render (Simulation _ extent scaleBy (WaTor wator)) =
-    return . uncurry translate (bimap slide slide extent)
+render (Simulation _ extent scaleBy (WaTor _ wator)) =
+    return . uncurry translate (slide `both` extent)
            . pictures
            . mapMaybe render'
-           . zip indexes
+           . zip indexes'
            . toList
            $ fmap entityColor wator
     where
-        (w, h)   = v2dExtent wator
         scaleBy' = fromIntegral scaleBy
-        indexes  = [ (x, y) | y <- [0..h-1], x <- [0..w-1] ]
+        indexes' = map floatPair . indexes $ v2dExtent wator
         slide x  = -1.0 * x
 
         render' (_     , Nothing) = Nothing
         render' ((x, y), Just c)  =
             Just . color c
-                 . translate (scaleBy' * fromIntegral x)
-                             (scaleBy' * fromIntegral y)
+                 . translate (scaleBy' * x) (scaleBy' * y)
                  $ circle (scaleBy' / 2.0)
 
 entityColor :: Entity -> Maybe Color
-entityColor Fish  = Just $ dim blue
-entityColor Shark = Just $ dim red
-entityColor Empty = Nothing
+entityColor Fish{}  = Just $ dim blue
+entityColor Shark{} = Just $ dim red
+entityColor Empty   = Nothing
 
-step :: ViewPort -> Float -> Simulation -> IO Simulation
-step _ _ = return
+step :: GenIO -> ViewPort -> Float -> Simulation -> IO Simulation
+step g _ _ s@(Simulation ps _ _ (WaTor t (V2D extent w))) = do
+    v' <- V2D extent <$> V.thaw w
+
+    ((`shuffle` g) $ V.zip indexes' w)
+        >>= V.mapM_ (uncurry (stepCell g ps extent v'))
+
+    v'' <- V.freeze $ v2dData v'
+    return $ s { wator = WaTor { time = t + 1
+                               , world = V2D extent v''
+                               }
+               }
+    where
+        indexes' = V.fromList $ indexes extent
+
+stepCell :: GenIO
+         -> Params
+         -> Coord
+         -> Vector2D MV.IOVector Entity
+         -> Coord
+         -> Entity
+         -> IO ()
+
+stepCell g Params{..} extent v pos f@Fish{} = do
+    mto <-  (`randomElem` g)
+        =<< filter (isEmpty . snd)
+        <$> neighborhoodEntities extent pos v
+    case mto of
+        Just (to, Empty)  ->
+            swapReproduce v pos to f' fishAge fishReproduce $ Fish 0
+        Just (_, Fish{})  -> return ()
+        Just (_, Shark{}) -> return ()
+        Nothing           -> return ()
+    where
+        f' = f { fishAge = fishAge f + 1 }
+
+stepCell g ps extent v pos@(x, y) Shark{..} = undefined
+stepCell _ _ _ _ _      Empty = return ()
+
+up, down, left, right :: Coord -> Coord -> Coord
+
+up (_, h) (x, 0) = (x, h - 1)
+up _      (x, y) = (x, y - 1)
+
+down (_, h) (x, y) | y == h - 1 = (x, 0)
+                   | otherwise  = (x, y + 1)
+
+left (w, _) (0, y) = (w - 1, y)
+left _      (x, y) = (x - 1, y)
+
+right (w, _) (x, y) | x == w - 1 = (0, y)
+                    | otherwise  = (x + 1, y)
+
+neighborhood :: Coord -> Coord -> [Coord]
+neighborhood extent center = map (\f -> f extent center) [up, right, down, left]
+
+neighborhoodEntities :: Coord -> Coord -> Vector2D MV.IOVector a
+                     -> IO [(Coord, a)]
+neighborhoodEntities extent center v =
+    fmap (mapMaybe sequenceA)
+        . mapM (sequenceA . (id &&& (v !!?)))
+        $ neighborhood extent center
+
+isFish, isShark, isEmpty :: Entity -> Bool
+
+isFish Fish{}   = True
+isFish _        = False
+
+isShark Shark{} = True
+isShark _       = False
+
+isEmpty Empty   = True
+isEmpty _       = False
+
+swapReproduce :: Vector2D MV.IOVector Entity
+              -> Coord
+              -> Coord
+              -> Entity
+              -> (Entity -> Chronon)
+              -> Chronon
+              -> Entity
+              -> IO ()
+swapReproduce v from to entity getAge reproduceAge child = undefined
+
+shuffle :: V.Vector a -> GenIO -> IO (V.Vector a)
+shuffle = undefined
+
+randomSwap :: V.Vector a -> Int -> GenIO -> IO a
+randomSwap = undefined
+
+randomElem :: [a] -> GenIO -> IO (Maybe a)
+randomElem = undefined
 
 
 main :: IO ()
 main = do
     ps  <- execParser opts
-    wtr <- withSystemRandom $ asGenIO $ \g ->
-        randomWaTor g ps (w, h)
-    simulateIO (InWindow "Wa-Tor" (w * scaleBy, h * scaleBy) (0, 0))
-               black
-               10
-               (Simulation ps (fromIntegral w, fromIntegral h) scaleBy wtr)
-               render
-               step
+    withSystemRandom $ asGenIO $ \g -> do
+        wtr <- randomWaTor g ps (w, h)
+        simulateIO (InWindow "Wa-Tor" (w * scaleBy, h * scaleBy) (0, 0))
+                   black
+                   7
+                   (Simulation ps (fromIntegral w, fromIntegral h) scaleBy wtr)
+                   render
+                   (step g)
     where
         w       = 500
         h       = 309
